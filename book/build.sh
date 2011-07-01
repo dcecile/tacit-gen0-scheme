3d25cd1bce8e301857cf0748a1cb652d1c823cc5
Dan Cecile <dancecile@gmail.com>
2011-07-01 18:00:33 -0400

    Done indexing notes up to today

--- book/build.sh
+++ book/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Requires texlive-core and texlive-latexextra
 xelatex -no-pdf master \
 && makeindex master \
 && bibtex master \
--- book/clean.sh
+++ book/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm master.aux master.bbl master.blg master.idx master.ilg master.ind master.log master.out master.pdf master.toc
+rm master.aux master.bbl master.blg master.idx master.ilg master.ind master.log master.out master.pdf master.toc *.aux
